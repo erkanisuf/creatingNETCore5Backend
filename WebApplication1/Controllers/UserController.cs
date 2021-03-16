9caaab127beec836e6fef9210a14c46964f26584
erkanisuf <63913743+erkanisuf@users.noreply.github.com>
2021-03-16 23:32:42 +0200

    forgotpassword

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -1,11 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 using System.IdentityModel.Tokens.Jwt;
@@ -15,6 +13,8 @@ using System.Text;
 using System.Threading.Tasks;
 using WebApplication1.Models;
 using Microsoft.Extensions.Configuration;
+using SendGrid.Helpers.Mail;
+using SendGrid;
 
 namespace WebApplication1.Controllers
 {
@@ -134,8 +134,60 @@ namespace WebApplication1.Controllers
             return Ok(new RegistrationResponseDto { IsSuccs = true, Email = userId, TokenExpires = expiration });
         }
 
+        [HttpPost]
+        // Checks users token and then looks inside the token and gets the name and other information.
+        public async Task<IActionResult> ForgotPassword([FromBody] string email)
+        {
+            //
+            var user = await _userManager.FindByNameAsync(email);
+            string kur = user.Id;
+            /*if (user == null)
+            {
+                // Don't reveal that the user does not exist or is not confirmed
+                return Ok(new RegistrationResponseDto { IsSuccs = true, Email = email })
+            }*/
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            string apiKey = Configuration.GetValue<string>("SendGrid:mySecret");
+            /*var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");*/ // Other possible way to use
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("djerimixer2260@abv.bg", "Helsinki Guide App");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress(email, "Password reset - Helsinki Guide App");
+            var plainTextContent = "Click on the button and after that add your new password";
+            var htmlContent = "<h1>Click on the button and after that add your new password</h1>" +
+                "<br /><button style='background-color:#0093e9;border:none;color:white;width:200px;padding:25px;cursor:Pointer;'>" +
+                "Click Here</button> " +
+                $"{token}";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            return Ok(new RegistrationResponseDto { IsSuccs = true, Email = kur });
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // Checks users token and then looks inside the token and gets the name and other information.
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordModel resetPasswordModel)
+        {
+            //check if valid model
+            var user = await _userManager.FindByEmailAsync(resetPasswordModel.Email);
+            var resetPassResult = await _userManager.ResetPasswordAsync(user, resetPasswordModel.Token, resetPasswordModel.Password);
+
+            //Do this more elegant way to response the error array!
+            if (!resetPassResult.Succeeded)
+            {
+                List<string> kur = new List<string>();
+                foreach (var error in resetPassResult.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                    kur.Add(error.Description);
+                }
+                return Ok(kur);
+            }
+            else { return Ok(new RegistrationResponseDto { IsSuccs = true, Email = user.Email, }); }
+        }
 
 
-       
-    }
+
+        }
 }
